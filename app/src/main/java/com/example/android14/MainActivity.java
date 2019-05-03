2e278140f9c39a25fb84e92443c94473c0646e94
JZhou717 <32233263+JZhou717@users.noreply.github.com>
2019-05-03 00:02:16 -0400

    Added start screen and laid out basic stuff for the game scree

--- app/src/main/java/com/example/android14/MainActivity.java
+++ app/src/main/java/com/example/android14/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android14;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button play_game_button = (Button) findViewById(R.id.play_game_button);
+
+        play_game_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
 
         
     }
